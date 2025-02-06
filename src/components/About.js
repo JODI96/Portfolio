9b9974b43210d3b028f0da37725e92c700036b0b
Jodi <joys96@gmail.com>
2025-02-06 22:24:10 +0100

    initial commit

---
+++ src/components/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <section>
+      <h2>About Me</h2>
+      <p>I am a software engineer and web developer with experience in building web applications using modern technologies.</p>
+    </section>
+  );
+};
+
+export default About;
